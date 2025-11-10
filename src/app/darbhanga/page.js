9e8855000d19add158ba91de6e36369f4e9cef88
Nikhil Kumar <nikhilsk369@gmail.com>
2025-11-10 22:36:07 +0530

    bottom icons bg color

--- src/app/darbhanga/page.js
+++ src/app/darbhanga/page.js
@@ -1,5 +1,21 @@
 "use client";
 import { Phone, Hotel, Coffee, ParkingSquare, Clock } from "lucide-react";
+import { Poppins, Playfair_Display, Barlow_Condensed } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+const barlow = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 import Image from "next/image";
 
 export default function DarbhangaPage() {
@@ -21,24 +37,30 @@ export default function DarbhangaPage() {
           {/* --- Top Nav --- */}
           <div className="flex justify-between items-center py-3 text-sm">
             {/* Left: Phone Number */}
-            <div className="flex items-center gap-2 text-[#c28e47]">
+            <div className="flex items-center gap-2 text-white">
               <Phone className="w-4 h-4" />
-              <span className="text-lg font-bold">+91 8130026761/ 62/ 63</span>
+              <span className="text-lg ">+91 8130026761/ 62/ 63</span>
             </div>
 
             {/* Right: Reservations / Gallery / Contact */}
-            <div className="flex items-center">
+            <div className={`flex items-center ${poppins.className}`}>
               {/* Vertical divider */}
               <div className="h-10 mt-5 w-[1px] bg-[#c28e47]/50 mx-6" />
 
-              <button className="bg-slate-800 text-white px-6 py-2 uppercase tracking-wide font-medium hover:bg-[#c28e47] hover:text-black transition">
+              <button className="bg-stone-800 text-white text-lg px-6 py-2 uppercase tracking-wide font-medium hover:bg-[#FAB000] hover:text-black transition">
                 Reservations
               </button>
 
-              <a href="#" className="ml-6 hover:text-[#c28e47] transition">
+              <a
+                href="#"
+                className="ml-6 text-xl hover:text-[#FAB000] transition"
+              >
                 Gallery
               </a>
-              <a href="#" className="ml-6 hover:text-[#c28e47] transition">
+              <a
+                href="#"
+                className="ml-6 text-xl hover:text-[#FAB000] transition"
+              >
                 Contact us
               </a>
             </div>
@@ -48,20 +70,37 @@ export default function DarbhangaPage() {
           <div className="h-[1px] mt-[-11.5] bg-[#c28e47]/40 w-full my-2"></div>
 
           {/* --- Bottom Nav --- */}
-          <nav className="flex justify-end items-center gap-10 py-3 uppercase text-white tracking-wide text-sm font-medium pr-10">
-            <a href="#" className="hover:text-[#c28e47] transition">
-              About Us
+          <nav
+            className={`${barlow.className} flex justify-end items-center gap-10 mt-3 py-3 text-xl font-[600] text-white tracking-wide pr-10`}
+          >
+            <a
+              href="#"
+              className="hover:underline underline-offset-16 decoration-[#FAB000] transition"
+            >
+              About us
             </a>
-            <a href="#" className="hover:text-[#c28e47] transition">
-              Weddings and Occasions
+            <a
+              href="#"
+              className="hover:underline underline-offset-16 decoration-[#FAB000] transition"
+            >
+              Weddings & Occasions
             </a>
-            <a href="#" className="hover:text-[#c28e47] transition">
+            <a
+              href="#"
+              className="hover:underline underline-offset-16 decoration-[#FAB000] transition"
+            >
               Our Rooms
             </a>
-            <a href="#" className="hover:text-[#c28e47] transition">
+            <a
+              href="#"
+              className="hover:underline underline-offset-16 decoration-[#FAB000] transition"
+            >
               Banquets & Venue
             </a>
-            <a href="#" className="hover:text-[#c28e47] transition">
+            <a
+              href="#"
+              className="hover:underline underline-offset-16 decoration-[#FAB000] transition"
+            >
               Dining
             </a>
           </nav>
@@ -87,20 +126,22 @@ export default function DarbhangaPage() {
           {/* Feature icons */}
           {features.map((item, i) => (
             <div key={i} className="relative flex flex-col items-center z-10">
-              <div className="relative flex items-center justify-center w-14 h-14 bg-[#032828] border-[2.5px] border-[#5ab6b8] rounded-full transition-all hover:bg-[#065e5f]">
+              <div className="relative flex items-center justify-center w-14 h-14 bg-[#032828] border-[2.5px] border-white rounded-full transition-all hover:bg-[#FAB000] hover:border-[#FAB000]">
                 {/* left line */}
                 {i >= 0 && (
-                  <div className="absolute left-[-16px] top-1/2 w-[16px] h-[3px] bg-[#5ab6b8] -translate-y-1/2" />
+                  <div className="absolute left-[-16px] top-1/2 w-[15px] h-[3px] bg-[#5ab6b8] hover:bg-[#FAB000] -translate-y-1/2" />
                 )}
                 {/* right line */}
                 {i <= features.length - 1 && (
-                  <div className="absolute right-[-16px] top-1/2 w-[16px] h-[3px] bg-[#5ab6b8] -translate-y-1/2" />
+                  <div className="absolute right-[-16px] top-1/2 w-[15px] h-[3px] bg-[#5ab6b8] hover:bg-[#FAB000] -translate-y-1/2" />
                 )}
 
                 {item.icon}
               </div>
 
-              <span className="text-sm mt-2 text-white">{item.label}</span>
+              <span className="text-sm mt-2 text-white uppercase">
+                {item.label}
+              </span>
             </div>
           ))}
         </div>
