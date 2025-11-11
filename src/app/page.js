bbca74d03511dd53aff32021ad99fb86bcfc7ef3
Nikhil Kumar <nikhilsk369@gmail.com>
2025-11-11 13:29:57 +0530

    third commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,9 @@
 import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import ProductSection from "@/components/ProductSection";
-import ContactForm from "@/components/ContactForm";
-import GallerySection from "@/components/GallerySection";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <AboutSection />
-      <ProductSection />
-      <GallerySection /> */}
     </>
   );
 }
